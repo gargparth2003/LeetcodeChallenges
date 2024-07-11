f88abff0629dace3982f52bebab6b32cad7d82f0
gargparth2003 <122970727+gargparth2003@users.noreply.github.com>
2024-07-11 10:52:35 +0530

    in cpp

---
+++ Day1/1190.ReverseSubstringsBetweenEachPairofParentheses.cpp
@@ -0,0 +1,36 @@
+
+class Solution {
+public:
+    std::string reverseParentheses(std::string s) {
+        int n = s.length();
+        std::stack<int> opened;
+        std::unordered_map<int, int> pair;
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == '(') {
+                opened.push(i);
+            } else if (s[i] == ')') {
+                int j = opened.top();
+                opened.pop();
+                pair[i] = j;
+                pair[j] = i;
+            }
+        }
+        std::string result;
+        int i = 0;
+        int direction = 1; 
+
+        while (i < n) {
+            if (s[i] == '(' || s[i] == ')') {
+                i = pair[i];
+                direction = -direction;
+            } else {
+                result.push_back(s[i]);
+            }
+            i += direction;
+        }
+
+        return result;
+    }
+};
+
+// link ---  https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses/description/?envType=daily-question&envId=2024-07-11
